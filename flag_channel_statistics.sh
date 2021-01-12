9682425302b379df3f25821159d3d2c907246b6d
Uky <38967345+uky007@users.noreply.github.com>
2021-01-12 12:19:08 +0900

    Update flag_channel_statistics.sh

--- flag_channel_statistics.sh
+++ flag_channel_statistics.sh
@@ -4,6 +4,6 @@ plot_date=$(date "+%Y-%m-%d")
 allviewCount=$(cat $(date "+%Y-%m-%d")_flag_channels_info.json | jq -r '.items[].statistics.viewCount')
 subscriberCount=$(cat $(date "+%Y-%m-%d")_flag_channels_info.json | jq -r '.items[].statistics.subscriberCount')
 
-target_file="channel_statistics.csv"
+target_file="flag_channel_statistics.csv"
 
 echo "${plot_date},${allviewCount},${subscriberCount}" >> ${target_file}
