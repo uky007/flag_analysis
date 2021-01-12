b74e4b7263793ecb91a89672a577a56770d2659d
Uky <38967345+uky007@users.noreply.github.com>
2021-01-12 13:26:04 +0900

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+path=$(pwd)
+
+sudo apt update -y
+sudo apt install vim emacs nmap curl jq python3 python3-pip -y
+sudo apt install fonts-takao -y
+pip3 install update
+pip3 install numpy pandas matplotlib
+
+mkdir $path/data
+mkdir $path/graph
+mkdir $path/datasets
+mkdir -p $path/thumbnail/default
+mkdir -p $path/thumbnail/medium
+mkdir -p $path/thumbnail/high
+mkdir -p $path/thumbnail/maxres
