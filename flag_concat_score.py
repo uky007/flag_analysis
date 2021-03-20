018eb76655635673e07043f4d5b3fddbce35981d
Uky <38967345+uky007@users.noreply.github.com>
2021-03-21 08:52:35 +0900

    Add files via upload

--- flag_concat_score.py
+++ flag_concat_score.py
@@ -14,8 +14,9 @@ from pandas.plotting import register_matplotlib_converters
 datasets_file = sys.argv[1]
 score_file = "flag_videos_score.csv"
 rank_file = "flag_rank.csv"
+anken_file = "flag_anken.csv"
 
-flag_header = ["vid", "title", "date", "description", "viewCount", "likeCount", "dislikeCount", "commentCount", "videoTime", "DeadEndCount", "shibouflagCount", "seizonflagCount", "renaiflagCount", "rank", "score"]
+flag_header = ["vid", "title", "date", "description", "viewCount", "likeCount", "dislikeCount", "commentCount", "videoTime", "DeadEndCount", "shibouflagCount", "seizonflagCount", "renaiflagCount", "rank", "anken", "score"]
 
 output_file = sys.argv[2]
 
@@ -26,8 +27,10 @@ score = pd.read_csv(score_file, header=None, index_col=0, encoding='UTF8')
 #print(score)
 rank = pd.read_csv(rank_file, header=None, index_col=None, na_values=['N/A'], encoding='UTF8')
 rank.index = np.arange(1, len(rank) + 1)
+anken = pd.read_csv(anken_file, header=None, index_col=None, na_values=['N/A'], encoding='UTF8')
+anken.index = np.arange(1, len(anken) + 1)
 #print(rank)
-concat_csv = pd.concat([datasets, rank, score], axis=1)
+concat_csv = pd.concat([datasets, rank, anken, score], axis=1)
 concat_csv.to_csv(output_file, header=True, index=True)
 
 #flag_header = pd.read_csv(flag_header_file, header=0, encoding='UTF8')
