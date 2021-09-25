a93159846de2d2769c916148444bcf2d155660cd
Uky <38967345+uky007@users.noreply.github.com>
2021-09-25 16:12:20 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,8 @@ LANG=C xdg-user-dirs-gtk-update
 sudo apt install vim emacs nmap curl jq python3 python3-pip -y
 sudo apt install fonts-takao -y
 sudo atp install graphviz -y
-sudo apt install librsvg2-bin 
+sudo apt install librsvg2-bin -y
+sudo apt install libopencv-dev python3-opencv -y
 pip3 install update
 pip3 install numpy pandas matplotlib sklearn dtreeviz
 
